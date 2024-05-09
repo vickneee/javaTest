288f3d81bd3d40f7b5627de685dabe4768d7b2ee
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-05-09 23:04:03 +0300

    Initial commit

---
+++ src/Case.java
@@ -0,0 +1,10 @@
+public class Case {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world: Peter Pan!");
+        System.out.println("Hello world: Peter Pan!");
+        System.out.println("Hello world: Peter Pan!");
+        System.out.println("Hello world: Peter Pan!");
+        System.out.println("Hello world: Peter Pan!");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Case.main(new String[0]);
+
+        System.out.println(3+4);
+        System.out.println(3*4);
+
+        String firstname = "Peter";
+        String lastName = "Pan";
+        firstname = "John";
+
+        int numOne = 3;
+        int numTwo = 4;
+        int result = numOne + numTwo;
+        System.out.println(result);
+
+        System.out.println("Hello world!!!!!!!: " + firstname + " " + lastName + "!");
+        // Single line comment
+        // data types
+        // int, double, float, char, boolean, String
+        // primitive data types
+        // int, double, float, char, boolean
+        // non-primitive data types
+        // String, Array, Class, Interface
+
+        /*
+        Multi-line comment
+         */
+
+
+    }
+}
